079ec87f6e86e538726dd6be5df21b5fff2fa6bb
Espen Hovlandsdal <espen@hovlandsdal.com>
2015-12-24 00:37:32 +0100

    Bring back outcommented code

--- src/element-pan.js
+++ src/element-pan.js
@@ -123,9 +123,9 @@ var ElementPan = React.createClass({
         this.el = this.refs.container;
 
         // Old versions of React doesn't return the raw DOM node
-       // if (!(this.el instanceof window.Node)) {
-       //     this.el = this.el.getDOMNode();
-       // }
+        if (!(this.el instanceof window.Node)) {
+            this.el = this.el.getDOMNode();
+        }
 
         if (this.props.startX) {
             this.el.scrollLeft = this.props.startX;
@@ -139,8 +139,8 @@ var ElementPan = React.createClass({
     getContainerStyles: function() {
         var style = {
             overflow: 'hidden',
-            cursor: 'move'            
-        }
+            cursor: 'move'
+        };
 
         if (this.props.width) {
             style.width = this.props.width;
